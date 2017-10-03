f989c0bccf9f74d0acdd5157390df9946e2a12f0
ilyaf f <ilyaf@appveyor.com>
2017-10-03 01:24:39 -0700

    Add nunit dotnet core test

---
+++ NunitDotNetCore/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NunitDotNetCore
+{
+    [TestFixture]
+    public class EnumToStringConverterTests
+    {
+        [Test]
+        public void NunitTest1()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
